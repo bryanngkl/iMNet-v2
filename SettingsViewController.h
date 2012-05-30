18e969490f2dc60b6700669442840aae54ab1cfc
Bryan Ng <bryan_ng_kl@hotmail.com>
2012-05-30 03:01:28 +0100

    Application Storyboard Skeleton (Bryan)

---
+++ SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  iMNet v2
+//
+//  Created by Bryan on 30/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UITableViewController
+
+@end
---
+++ iMNet v2/PersonalInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonalInfoViewController.h
+//  iMNet v2
+//
+//  Created by Bryan on 30/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonalInfoViewController : UITableViewController
+
+@end
